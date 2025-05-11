e8d46c1b090ac711fba2a958da7f1cb4d2e61841
Avenger7225 <angelhirales16@gmail.com>
2025-05-10 17:04:17 -0700

    agregamos iconos y estilos

--- navigation/Tabs.tsx
+++ navigation/Tabs.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialIcons } from 'react-native-vector-icons';
+import { StyleSheet } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import HomeScreen from '../screens/HomeScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import FamilyScreen from '../screens/FamilyScreen';
@@ -9,13 +10,33 @@ const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator 
+      screenOptions={{
+        tabBarItemStyle: {
+          width: "80%",
+          height: "80%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "white",
+          borderRadius: 40,
+          paddingTop: 2.5,
+          marginHorizontal: 20,
+          marginBottom: 20,
+          height: 60,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 2,
+          borderColor: "white",
+        },
+      }}>
       <Tab.Screen 
         name="Home" 
         component={HomeScreen} 
         options={{
             tabBarIcon: ({ focused }) => (
-              <MaterialIcons name="star" size={30} color={focused ? 'blue' : 'gray'} />
+              <Ionicons name="star" size={30} color={focused ? 'blue' : 'gray'} />
             ),
         }}
       />
@@ -24,7 +45,7 @@ export default function Tabs() {
         component={ProfileScreen} 
         options={{
             tabBarIcon: ({ focused }) => (
-              <MaterialIcons name="person" size={30} color={focused ? 'blue' : 'gray'} />
+              <Ionicons name="person" size={30} color={focused ? 'blue' : 'gray'} />
             ),
         }}
       />
@@ -33,10 +54,13 @@ export default function Tabs() {
         component={FamilyScreen} 
         options={{
             tabBarIcon: ({ focused }) => (
-              <MaterialIcons name="group" size={30} color={focused ? 'blue' : 'gray'} />
+              <Ionicons name="people" size={30} color={focused ? 'blue' : 'gray'} />
             ),
         }}
       />
     </Tab.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+});
