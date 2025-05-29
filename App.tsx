05017f640a342863197afb45eecb837388410918
Avenger7225 <angelhirales16@gmail.com>
2025-05-28 22:59:09 -0700

    se mejoro la funcionalidad general

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { PermissionsAndroid, Platform } from 'react-native';
+
 import { UbicacionProvider } from './context/UbicacionContext';
 import { BluetoothProvider } from './context/BluetoothContext';
 import AppStack from './navigation/Stack';
 
 export default function App() {
+
+  useEffect(() => {
+    async function solicitarPermisosBluetooth() {
+      if (Platform.OS === 'android' && Platform.Version >= 31) {
+        try {
+          const granted = await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          ]);
+
+          if (
+            !['never_ask_again', 'granted'].includes(granted['android.permission.BLUETOOTH_SCAN']) ||
+            !['never_ask_again', 'granted'].includes(granted['android.permission.BLUETOOTH_CONNECT'])||
+            !['never_ask_again', 'granted'].includes(granted['android.permission.ACCESS_FINE_LOCATION'])
+          ) {
+            console.log('✅ Todos los permisos concedidos');
+          } else {
+            console.warn('❌ Faltan permisos de Bluetooth o ubicación');
+          }
+        } catch (err) {
+          console.error('Error solicitando permisos:', err);
+        }
+      }
+    }
+
+    solicitarPermisosBluetooth();
+  }, []);
+
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>    
-        <UbicacionProvider>
-          <NavigationContainer>
-            <BluetoothProvider>
-              <AppStack />
-            </BluetoothProvider>
-          </NavigationContainer>
-        </UbicacionProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <UbicacionProvider>
+        <NavigationContainer>
+          <BluetoothProvider>
+            <AppStack />
+          </BluetoothProvider>
+        </NavigationContainer>
+      </UbicacionProvider>
     </GestureHandlerRootView>
   );
 }
